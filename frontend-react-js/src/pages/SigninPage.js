0f5bbfc261cd7113182bab503a72fa1d4e6bfc66
franklin-moux <59755163+devtest4me@users.noreply.github.com>
2023-04-05 09:52:31 -1000

    Update SigninPage.js

--- frontend-react-js/src/pages/SigninPage.js
+++ frontend-react-js/src/pages/SigninPage.js
@@ -4,8 +4,8 @@ import {ReactComponent as Logo} from '../components/svg/logo.svg';
 import { Link } from "react-router-dom";
 
 // [TODO] Authenication
+// import Cookies from 'js-cookie'
 import { Auth } from 'aws-amplify';
-
 export default function SigninPage() {
 
   const [email, setEmail] = React.useState('');
@@ -14,21 +14,20 @@ export default function SigninPage() {
 
   const onsubmit = async (event) => {
     setErrors('')
-    // console.log('onsubmit')
     event.preventDefault();
     Auth.signIn(email, password)
-    .then(user => {
-      console.log("user", user)
-      localStorage.setItem("access_token", user.signInUserSession.accessToken.jwtToken)
-      window.location.href = "/"
-    })
-    .catch(error => { 
-      if (error.code == 'UserNotConfirmedException') {
-      window.location.href = "/confirm"
-      }
-      setErrors(error.message)  
+      .then(user => {
+        console.log ('user', user)
+        localStorage.setItem("access_token", user.signInUserSession.accessToken.jwtToken)
+        window.location.href = "/"
+      })
+      .catch(error => { 
+        if (error.code == 'UserNotConfirmedException') {
+          window.location.href = "/confirm"
+        }
+      setErrors(error.message)
     });
-   return false
+    return false
   }
 
   const email_onchange = (event) => {
