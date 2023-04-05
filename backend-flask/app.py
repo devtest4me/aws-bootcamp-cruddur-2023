930607698136d7b196bb49a3a2705f9970c747a5
franklin-moux <59755163+devtest4me@users.noreply.github.com>
2023-04-05 09:32:23 -1000

    Update app.py

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -198,23 +198,21 @@ def data_create_message():
       return {}, 401
 
 @app.route("/api/activities/home", methods=['GET'])
-# @xray_recorder.capture('activities_home')
-# @aws_auth.authentication_required
+@xray_recorder.capture('activities_home')
 def data_home():
   access_token = extract_access_token(request.headers)
   try:
-    claims = cognito_token_verify.verify(access_token)
-    user_id = claims["username"]
-    # authenticated request
-    app.logger.debug('authenticated')
-    app.logger.debug(claims["username"])
-    data = HomeActivities.run(cognito_user_id=user_id)
+    claims = cognito_jwt_token.verify(access_token)
+    # authenicatied request
+    app.logger.debug("authenicated")
+    app.logger.debug(claims)
+    app.logger.debug(claims['username'])
+    data = HomeActivities.run(cognito_user_id=claims['username'])
   except TokenVerifyError as e:
-    # unauthenticated request
+    # unauthenicatied request
     app.logger.debug(e)
-    app.logger.debug('unauthenticated')
+    app.logger.debug("unauthenicated")
     data = HomeActivities.run()
-
   return data, 200
 
 @app.route("/api/activities/notifications", methods=['GET'])
