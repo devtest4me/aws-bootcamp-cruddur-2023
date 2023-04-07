65856d939ad5f9395ed60b998cf8f132fcdec3af
franklin-moux <59755163+devtest4me@users.noreply.github.com>
2023-04-07 13:17:58 -1000

    Update HomeFeedPage.js

--- frontend-react-js/src/pages/HomeFeedPage.js
+++ frontend-react-js/src/pages/HomeFeedPage.js
@@ -1,13 +1,16 @@
 import './HomeFeedPage.css';
 import React from "react";
-import {checkAuth, getAccessToken} from '../lib/CheckAuth';
+
+import { Auth } from 'aws-amplify';
+
 import DesktopNavigation  from '../components/DesktopNavigation';
 import DesktopSidebar     from '../components/DesktopSidebar';
 import ActivityFeed from '../components/ActivityFeed';
 import ActivityForm from '../components/ActivityForm';
 import ReplyForm from '../components/ReplyForm';
+
 // [TODO] Authenication
-// import Cookies from 'js-cookie'
+import Cookies from 'js-cookie'
 
 export default function HomeFeedPage() {
   const [activities, setActivities] = React.useState([]);
@@ -20,12 +23,7 @@ export default function HomeFeedPage() {
   const loadData = async () => {
     try {
       const backend_url = `${process.env.REACT_APP_BACKEND_URL}/api/activities/home`
-      await getAccessToken()
-      const access_token = localStorage.getItem("access_token")
       const res = await fetch(backend_url, {
-        headers: {
-          Authorization: `Bearer ${access_token}`
-        },
         method: "GET"
       });
       let resJson = await res.json();
@@ -39,10 +37,31 @@ export default function HomeFeedPage() {
     }
   };
 
-  // check when the page loads if we are authenicated
+  const checkAuth = async () => {
+    Auth.currentAuthenticatedUser({
+      // Optional, By default is false. 
+      // If set to true, this call will send a 
+      // request to Cognito to get the latest user data
+      bypassCache: false 
+    })
+    .then((user) => {
+      console.log('user',user);
+      return Auth.currentAuthenticatedUser()
+    }).then((cognito_user) => {
+        setUser({
+          display_name: cognito_user.attributes.name,
+          handle: cognito_user.attributes.preferred_username
+        })
+    })
+    .catch((err) => console.log(err));
+  };
   React.useEffect(()=>{
+    //prevents double call
+    if (dataFetchedRef.current) return;
+    dataFetchedRef.current = true;
+
     loadData();
-    checkAuth(setUser);
+    checkAuth();
   }, [])
 
   return (
@@ -50,7 +69,6 @@ export default function HomeFeedPage() {
       <DesktopNavigation user={user} active={'home'} setPopped={setPopped} />
       <div className='content'>
         <ActivityForm  
-          user_handle={user}
           popped={popped}
           setPopped={setPopped} 
           setActivities={setActivities} 
